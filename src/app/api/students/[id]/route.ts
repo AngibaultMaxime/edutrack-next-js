f7a6844a6a942d302bb7a6cbbde0019af4b8529b
AngibaultMaxime <maxime.angibault@gmail.com>
2025-10-28 14:39:10 -0400

    GET, PUT et DELETE pour students + tests

---
+++ src/app/api/students/[id]/route.ts
@@ -0,0 +1,93 @@
+import { prisma } from "@/lib/prisma";
+import { Prisma } from "@prisma/client";
+import { NextResponse } from "next/server";
+import z from "zod";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+
+  try {
+    const student = await prisma.student.findUnique({ where: { id } });
+    return NextResponse.json(student);
+  } catch (error) {
+    if (
+      error instanceof Prisma.PrismaClientKnownRequestError &&
+      error.code === "P2025"
+    )
+      return NextResponse.json(
+        { error: "Étudiant non trouvé" },
+        { status: 404 }
+      );
+    return NextResponse.json(
+      { error: "Impossible de trouver l'étudiant." },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PUT(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+
+  try {
+    const body = await req.json();
+
+    const student = await prisma.student.update({ where: { id }, data: body });
+    return NextResponse.json(student);
+  } catch (error) {
+    if (error instanceof z.ZodError)
+      return NextResponse.json(
+        { error: "Erreur de format. Impossible de mettre à jour l'étudiant." },
+        { status: 400 }
+      );
+
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2025")
+        return NextResponse.json(
+          { error: "Etudiant non trouvé" },
+          { status: 404 }
+        );
+
+      if (error.code === "P2002")
+        return NextResponse.json(
+          { error: "Un étudiant avec cet email existe déjà." },
+          { status: 409 }
+        );
+    }
+
+    return NextResponse.json(
+      { error: "Impossible de mettre à jour l'étudiant." },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+
+  try {
+    const etudiant = await prisma.student.delete({ where: { id } });
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    if (
+      error instanceof Prisma.PrismaClientKnownRequestError &&
+      error.code === "P2025"
+    )
+      return NextResponse.json(
+        { error: "Étudiant non trouvé" },
+        { status: 404 }
+      );
+    return NextResponse.json(
+      { error: "Impossible de supprimer l'étudiant." },
+      { status: 500 }
+    );
+  }
+}
