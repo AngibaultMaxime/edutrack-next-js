d5c88c105d7858bb0fafb5d2c9236d90913cdaa0
AngibaultMaxime <maxime.angibault@gmail.com>
2025-10-29 14:26:22 -0400

    Catch d'erreurs pour l'enregistrement

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -2,40 +2,68 @@ import { prisma } from "@/lib/prisma";
 import bcrypt from "bcryptjs";
 import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
+import z from "zod";
 
 const JWT_SECRET = process.env.JWT_SECRET!;
 
 export async function POST(req: Request) {
-    const { email, password } = await req.json();
+  const { email, password } = await req.json();
 
+  try {
     // Verifie que l'utilisateur existe
     const user = await prisma.user.findUnique({ where: { email } });
     if (!user) {
-        return NextResponse.json({ error: "Aucun utilisateur existant avec cet email."}, { status: 401});
-
+      return NextResponse.json(
+        { error: "Aucun utilisateur existant avec cet email." },
+        { status: 401 }
+      );
     }
 
     // Verifie que le mot de passe est correct
     const valid = await bcrypt.compare(password, user.passwordHash);
     if (!valid) {
-        return NextResponse.json({ error: "Mot de passe incorrect."}, { status: 401 });
+      return NextResponse.json(
+        { error: "Mot de passe incorrect." },
+        { status: 401 }
+      );
     }
 
     // Genere le JWT
     const token = jwt.sign(
-        { id: user.id, email: user.email, role: user.role },
-        JWT_SECRET,
-        { expiresIn: "7d"}  // durée validité du token
+      { id: user.id, email: user.email, role: user.role },
+      JWT_SECRET,
+      { expiresIn: "7d" } // durée validité du token
     );
 
     // Retourne le token au client
     return NextResponse.json({
-        message: "Connexion réussie",
-        token,
-        user: {
-            id: user.id,
-            email: user.email,
-            role: user.role,
-        },
+      message: "Connexion réussie",
+      token,
+      user: {
+        id: user.id,
+        email: user.email,
+        role: user.role,
+      },
     });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      const fieldErrors = error.issues.map((issue) => ({
+        field: issue.path.join("."),
+        message: issue.message,
+      }));
+
+      return NextResponse.json(
+        {
+          error: "Erreur de format. Impossible de créer le cours.",
+          details: fieldErrors,
+        },
+        { status: 400 }
+      );
+    }
+
+    return NextResponse.json(
+      { error: "Impossible de créer l'utilisateur." },
+      { status: 500 }
+    );
+  }
 }
