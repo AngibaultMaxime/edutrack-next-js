634e4cdca3fd98d6f32050c0443bcdae24e5e0e1
AngibaultMaxime <maxime.angibault@gmail.com>
2025-10-29 12:42:15 -0400

    Enrollments + Tests

--- src/app/api/enrollments/[id]/route.ts
+++ src/app/api/enrollments/[id]/route.ts
@@ -1,1 +1,93 @@
-// TO DO
+import { prisma } from "@/lib/prisma";
+import { Prisma } from "@prisma/client";
+import { NextResponse } from "next/server";
+import z from "zod";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+
+  try {
+    const enrollment = await prisma.enrollment.findUnique({ where: { id } });
+
+    if (!enrollment)
+      return NextResponse.json(
+        { error: "Enrollment non trouvé." },
+        { status: 404 }
+      );
+
+    return NextResponse.json(enrollment);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Impossible de trouver l'enrollment." },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PUT(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+
+  try {
+    const body = await req.json();
+
+    const enrollment = await prisma.enrollment.update({
+      where: { id },
+      data: body,
+    });
+    return NextResponse.json(enrollment);
+  } catch (error) {
+    if (error instanceof z.ZodError)
+      return NextResponse.json(
+        {
+          error: "Erreur de format. Impossible de mettre à jour l'enrollment.",
+        },
+        { status: 400 }
+      );
+
+    if (
+      error instanceof Prisma.PrismaClientKnownRequestError &&
+      error.code === "P2025"
+    )
+      return NextResponse.json(
+        { error: "Enrollment non trouvé." },
+        { status: 404 }
+      );
+
+    return NextResponse.json(
+      { error: "Impossible de mettre à jour l'enrollment." },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+
+  try {
+    await prisma.enrollment.delete({ where: { id } });
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    if (
+      error instanceof Prisma.PrismaClientKnownRequestError &&
+      error.code === "P2025"
+    )
+      return NextResponse.json(
+        { error: "Enrollment non trouvé." },
+        { status: 404 }
+      );
+
+    return NextResponse.json(
+      { error: "Impossible de supprimer l'enrollment." },
+      { status: 500 }
+    );
+  }
+}
--- src/app/api/enrollments/route.ts
+++ src/app/api/enrollments/route.ts
@@ -1,1 +1,57 @@
-// TO DO
+import { prisma } from "@/lib/prisma";
+import { createEnrollmentSchema } from "@/lib/validation/enrollment";
+import { Prisma } from "@prisma/client";
+import { NextResponse } from "next/server";
+import z from "zod";
+
+export async function GET(req: Request) {
+  try {
+    const enrollments = await prisma.enrollment.findMany({
+      orderBy: { createdAt: "desc" },
+    });
+    return NextResponse.json(enrollments);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Impossible de recuperer les enrollments." },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const enrollmentData = createEnrollmentSchema.parse(body);
+
+    const enrollment = await prisma.enrollment.create({
+      data: {
+        courseId: enrollmentData.courseId,
+        studentId: enrollmentData.studentId,
+        status: enrollmentData.status,
+        grade: enrollmentData.grade,
+      },
+    });
+    return NextResponse.json({ success: true, enrollment}, { status: 201 });
+  } catch (error) {
+    if (error instanceof z.ZodError)
+      return NextResponse.json(
+        { error: "Erreur de format. Impossible de créer l'enrollment." },
+        { status: 400 }
+      );
+
+    if (
+      error instanceof Prisma.PrismaClientKnownRequestError &&
+      error.code === "P2003"
+    )
+      return NextResponse.json(
+        { error: "Le cours et/ou l'étudiant spécifié(s) n'existe(nt) pas." },
+        { status: 400 }
+      );
+
+    return NextResponse.json(
+      { error: "Impossible de créer l'enrollment." },
+      { status: 500 }
+    );
+  }
+}
